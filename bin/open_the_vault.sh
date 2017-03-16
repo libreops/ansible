98d52ca9c8b6158499e2be79be0b0ed535d8c28b
Nikos Roussos <nikos@roussos.cc>
2017-03-16 21:27:09 +0200

    Clean up repo
    * Make sure we use gpg2
    * Fix roles path
    * Adjust task to update admins access
    * Update admins
    * Remove duplicate vars

--- bin/open_the_vault.sh
+++ bin/open_the_vault.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # https://blog.andrewlorente.com/p/using-pgp-to-encrypt-the-ansible-vault
-gpg --batch --use-agent --decrypt vault-passwd.txt.gpg
+gpg2 --batch --use-agent --decrypt vault-passwd.txt.gpg
